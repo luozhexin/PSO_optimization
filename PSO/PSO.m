b7c5f60659df5786145ebe47ef5f953f176cf85f
luozhexin <44456331+luozhexin@users.noreply.github.com>
2020-06-05 12:46:05 +0800

    add

--- PSO/PSO.m
+++ PSO/PSO.m
@@ -1,69 +1,79 @@
- clear all
-close all
-clc
+clc;clear all;close all;
+tic;
 figure
 set(gcf,'Renderer','OpenGL');
  
-x = linspace(-10,10,30);
-y = linspace(-10,10,30);
-[x_new, y_new] = meshgrid(x,y);
+%x = linspace(-10,10,30);
+%y = linspace(-10,10,30);
+%x = linspace(-5,5,100);
+%y = linspace(-5,5,100);
+x=[-5:0.1:5];
+y=x;
+[x_new, y_new] = meshgrid(x,y);%生成网络采样点
 for k1 = 1: size(x_new, 1)
     for k2 = 1 : size(x_new , 2)
         X = [ x_new(k1,k2) , y_new(k1, k2) ];
-        z(k1,k2) = Ackley( X );
+        z(k1,k2) = Ackley( X );%代入Ackley函数
     end
 end
-h = contour(x_new, y_new, z , 20);
+h = contour(x_new, y_new, z , 20);%绘制等高线
  set(gca,'XLimMode','manual');
 set(gca,'YLimMode','manual');
-axis([-10,10,-10,10])
+%set(gca,'XLim',[-5,5]);
+%set(gca,'YLim',[-5,5]);
+%axis([-10,10,-10,10])
+axis([-5,5,-5,5])
 hold on
 view(2)
-shading interp
+shading interp %色彩平滑
+noP =36;%粒子群规模
+nVar = 2;%目标函数的自变量个数
 %set(h,'EraseMode','normal')
-noP =36;
-nVar = 2;
+%%
 % Objective function details
 fobj = @Ackley;
-lb = -10 * ones(1,nVar) ;
-ub = 10 * ones(1,nVar);
+lb = -5 * ones(1,nVar) ;
+ub = 5 * ones(1,nVar);
+%%
 % PSO paramters
-Max_iteration = 100;
-Vmax=6;
-wMax=0.9;
-wMin=0.2;
-c1=2;
-c2=2;
-cg_curve = zeros(1,Max_iteration);
-x = -10:4:10;
+Max_iteration = 100;%粒子最大迭代数
+Vmax=6;%粒子的最大飞行速度
+wMax=0.9;%最大惯性因子
+wMin=0.2;%最小惯性因子
+c1=2;%个体学习因子
+c2=2;%社会学习因子
+cg_curve = zeros(1,Max_iteration);%初始化收敛曲线convergence
+%x = -10:4:10;
+x = -5:2:5;%在给定区间[-5，5]里取点
 y= x;
 idx = 1;
-show_vel = 1;
+% show_vel = 1;%显示速度
 for t1 = 1: length (x)
     for t2 =  1: length (y)
         Swarm.Particles(idx).X = [x(t1) y(t2)];
         idx = idx+1;
     end
 end
+%%
 % Initializations
 for k = 1: noP
-    Swarm.Particles(k).V = rand(1,nVar);
+    Swarm.Particles(k).V = rand(1,nVar);%每个粒子速度随机分配
     Swarm.Particles(k).PBEST.X = rand(1,nVar)*Vmax;
     Swarm.Particles(k).PBEST.O= inf; % for minimization problems
     h(k) = plot(Swarm.Particles(k).X(1),Swarm.Particles(k).X(2),'ok', 'markerFaceColor','k')
-    set(h(k),'EraseMode','normal')
-    if show_vel == 1
-        p1 = [Swarm.Particles(k).X(1) Swarm.Particles(k).X(2)];                         % First Point
-        p2 = [Swarm.Particles(k).V(1) Swarm.Particles(k).V(2)];                         % Second Point
-        dp = p2-p1/100;                         % Difference
-        
-        %h2(k) = plot([p1(1) dp(1)],[p1(2) dp(2)],'-k' , 'LineWidth',2.5)
-        %set(h2(k),'EraseMode','normal')
-    end
+    %set(h(k),'EraseMode','normal')
+%     if show_vel == 1
+%         p1 = [Swarm.Particles(k).X(1) Swarm.Particles(k).X(2)];                         % First Point
+%         p2 = [Swarm.Particles(k).V(1) Swarm.Particles(k).V(2)];                         % Second Point
+%         dp = p2-p1/100;                         % Difference
+%         
+%         %h2(k) = plot([p1(1) dp(1)],[p1(2) dp(2)],'-k' , 'LineWidth',2.5)
+%         %set(h2(k),'EraseMode','normal')
+%     end
 end
 Swarm.GBEST.X=zeros(1,nVar);
 Swarm.GBEST.O= inf;
-
+%%
 for t=1:Max_iteration % main loop
     for k=1:noP
         %Calculate objective function for each particle
@@ -97,7 +107,7 @@ for t=1:Max_iteration % main loop
         saveas(gcf,'100.jpg');
     end
     %Update the inertia weight
-    w=wMax-t*((wMax-wMin)/Max_iteration);
+    w=wMax-t*((wMax-wMin)/Max_iteration);%惯性权重由wMax递减到wMin
     
     %Update the Velocity and Position of particles
     for k=1:noP
@@ -115,21 +125,27 @@ for t=1:Max_iteration % main loop
         Swarm.Particles(k).V(index) = -Vmax*rand;
         
         first_loc(k,:) = Swarm.Particles(k).X ;
-        Swarm.Particles(k).X = Swarm.Particles(k).X + Swarm.Particles(k).V;
+        if(Swarm.Particles(k).X + Swarm.Particles(k).V> ub)
+            Swarm.Particles(k).X=ub(1);
+        elseif(Swarm.Particles(k).X + Swarm.Particles(k).V < lb)
+            Swarm.Particles(k).X=lb(1);
+        else     
+            Swarm.Particles(k).X = Swarm.Particles(k).X + Swarm.Particles(k).V;
+        end
         second_loc(k,:) = Swarm.Particles(k).X ;
         
-        indx = find( Swarm.Particles(k).X  > ub);
-        Swarm.Particles(k).X (indx) = ub(1);
+        %indx = find( Swarm.Particles(k).X  > ub);
+        %Swarm.Particles(k).X (indx) = ub(1);
         
-        indx = find( Swarm.Particles(k).X  < lb);
-        Swarm.Particles(k).X (indx) = lb(1);
+        %indx = find( Swarm.Particles(k).X  < lb);
+        %Swarm.Particles(k).X (indx) = lb(1);
         
         nn = 20;
         moving_x(k,:) =  linspace(first_loc(k,1),second_loc(k,1),nn);
         moving_y(k,:) = linspace(first_loc(k,2),second_loc(k,2),nn);
         
-        moving_vx(k,:) =  linspace(first_vel(k,1),second_vel(k,1),nn);
-        moving_vy(k,:) = linspace(first_vel(k,2),second_vel(k,2),nn);
+        %moving_vx(k,:) =  linspace(first_vel(k,1),second_vel(k,1),nn);
+        %moving_vy(k,:) = linspace(first_vel(k,2),second_vel(k,2),nn);
         
     end
     
@@ -138,13 +154,13 @@ for t=1:Max_iteration % main loop
         for JJ = 1:noP;
             set(h(JJ),'XData', moving_x(JJ,rr),'YData', moving_y(JJ,rr));
             
-            if show_vel == 1
-                p1 = [moving_x(JJ,rr) moving_y(JJ,rr)];                         % First Point
-                p2 = [moving_x(JJ,end) moving_y(JJ,end)];
-                dp = (p2+p1)/2;                         % Difference
-                dp = (p1+dp)/2;
-                %set(h2(JJ),'XData', [p1(1), dp(1)],'YData',  [p1(2), dp(2)]);
-            end
+%             if show_vel == 1
+%                 p1 = [moving_x(JJ,rr) moving_y(JJ,rr)];                         % First Point
+%                 p2 = [moving_x(JJ,end) moving_y(JJ,end)];
+%                 dp = (p2+p1)/2;                         % Difference
+%                 dp = (p1+dp)/2;
+%                 %set(h2(JJ),'XData', [p1(1), dp(1)],'YData',  [p1(2), dp(2)]);
+%             end
         end
         
         drawnow
@@ -155,3 +171,4 @@ end
 figure
 plot(cg_curve)
 xlabel('Iteration')
+toc;
